74ff162fd49e493ec6dddbb33b8aff182351066c
mikeplattcapco <mike.platt@capco.com>
2014-07-28 11:13:42 +0100

    Initial Commit

---
+++ MJPSlider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MJPSlider
+//
+//  Created by Mike Platt on 28/07/2014.
+//  Copyright (c) 2014 Capco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
